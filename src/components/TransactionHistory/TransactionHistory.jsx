4ba82425d5f0409e7bdc71baa6348c89fe91ab14
Alexey-Kushnir <al7787@gmail.com>
2022-12-14 12:15:28 +0200

    transactionHistory

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { TransactionItem } from 'components/TransactionItem/TransactionItem';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(item => (
+          <TransactionItem key={item.id} {...item} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.array.isRequired,
+};
